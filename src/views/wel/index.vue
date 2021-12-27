c0201fcf15b66925692c5d86dc49b5ecdce5cba6
Inori <1120006860@qq.com>
2021-12-28 00:14:53 +0800

    first commit

---
+++ src/views/wel/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  import {mapGetters} from "vuex";
+
+  export default {
+    name: "wel",
+    data() {
+      return {
+        activeNames: ['1', '2', '3', '5'],
+        logActiveNames: ['27']
+      };
+    },
+    computed: {
+      ...mapGetters(["userInfo"]),
+    },
+    created() {
+    },
+    methods: {
+      handleChange(val) {
+        console.log(val);
+      }
+    }
+  };
+</script>
+
+<style>
+  .el-divider--horizontal {
+    margin: 12px 0 !important;
+  }
+
+  .el-font-size {
+    font-size: 14px;
+  }
+</style>
+
